df6b4fa360b4677fca3f359d97e51bd4a3792134
Gabriel Red <42196149+AllRedCat@users.noreply.github.com>
2025-04-07 05:16:16 +0000

    feat: adicionar páginas About, Blog, Contact e Projects com conteúdo inicial

---
+++ app/contact/page.tsx
@@ -0,0 +1,9 @@
+export default function Home() {
+  return (
+    <main className="bg-black flex h-dvh items-center justify-center p-24">
+      <h1 className="text-white text-3xl font-bold">
+        Contato
+      </h1>
+    </main>
+  )
+}
--- app/navbar.tsx
+++ app/navbar.tsx
@@ -1,40 +1,41 @@
+import Link from "next/link";
 import { House, FolderOpenDot } from "lucide-react";
 
 export default function Navbar() {
-  return (
-    <nav className="flex items-center justify-center p-4 border-2 border-gray-500 rounded-lg">
-        <ul className="flex">
-            <li className="mx-4">
-                <a href="/" className="text-gray-300 hover:text-gray-100">
-                    <House className="inline mr-2" />
-                    Home
-                </a>
-            </li>
-            <li className="mx-4">
-                <a href="/projects" className="text-gray-300 hover:text-gray-100">
-                    <FolderOpenDot className="inline mr-2" />
-                    Projects
-                </a>
-            </li>
-            <li className="mx-4">
-                <a href="/blog" className="text-gray-300 hover:text-gray-100">
-                    <FolderOpenDot className="inline mr-2" />
-                    Blog
-                </a>
-            </li>
-            <li className="mx-4">
-                <a href="/about" className="text-gray-300 hover:text-gray-100">
-                    <FolderOpenDot className="inline mr-2" />
-                    About
-                </a>
-            </li>
-            <li className="mx-4">
-                <a href="/contact" className="text-gray-300 hover:text-gray-100">
-                    <FolderOpenDot className="inline mr-2" />
-                    Contact
-                </a>
-            </li>
-        </ul>
-    </nav>
-  )
+    return (
+        <nav className="flex items-center justify-center p-4 border-2 border-gray-500 rounded-lg">
+            <ul className="flex">
+                <li className="mx-4">
+                    <Link href="/" className="text-gray-300 hover:text-gray-100">
+                        <House className="inline mr-2" />
+                        Home
+                    </Link>
+                </li>
+                <li className="mx-4">
+                    <Link href="/projects" className="text-gray-300 hover:text-gray-100">
+                        <FolderOpenDot className="inline mr-2" />
+                        Projetos
+                    </Link>
+                </li>
+                <li className="mx-4">
+                    <Link href="/blog" className="text-gray-300 hover:text-gray-100">
+                        <FolderOpenDot className="inline mr-2" />
+                        Blog
+                    </Link>
+                </li>
+                <li className="mx-4">
+                    <Link href="/about" className="text-gray-300 hover:text-gray-100">
+                        <FolderOpenDot className="inline mr-2" />
+                        Sobre
+                    </Link>
+                </li>
+                <li className="mx-4">
+                    <Link href="/contact" className="text-gray-300 hover:text-gray-100">
+                        <FolderOpenDot className="inline mr-2" />
+                        Contato
+                    </Link>
+                </li>
+            </ul>
+        </nav>
+    )
 }
